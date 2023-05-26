b6f9dd06c0d2bb2b10d29f4600ca7857ed361ec0
ShababulAlam <sababul37@gmail.com>
2023-05-27 00:39:25 +0600

    :memo: Add: dynamic style

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,5 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-
     <ExpandOnTap />
   </div>
 </template>
@@ -15,7 +13,7 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -23,5 +21,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.bg-aqua {
+  background-color: #e2e8f0;
 }
 </style>
--- src/components/ExpandOnTap.vue
+++ src/components/ExpandOnTap.vue
@@ -1,19 +1,157 @@
 <template>
   <div>
-    <div>Expandable {{ text }}</div>
+    <div :style="previewHeightCss" class="exanpdable relative overflow-clip">
+      <slot />
+      <div
+        v-if="!expandDetailsSection"
+        class="absolute bottom-0 w-full bg-gradient-fade flex justify-center"
+        :style="exapndOpenSectionStyle"
+        role="button"
+        @click="
+          () => {
+            expandDetailsSection = !expandDetailsSection;
+          }
+        "
+      >
+        <p class="">
+          {{ placeholderClosed }}
+        </p>
+        <div class="flex items-center">
+          <svg
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M6.351 8.751a1.2 1.2 0 0 1 1.698 0L12 12.703l3.951-3.952a1.2 1.2 0 1 1 1.697 1.697l-4.8 4.8a1.2 1.2 0 0 1-1.697 0l-4.8-4.8a1.2 1.2 0 0 1 0-1.697Z"
+              fill="#303339"
+            />
+          </svg>
+        </div>
+      </div>
+
+      <div
+        v-else
+        class="w-full flex justify-center"
+        :style="exapndCloseSectionStyle"
+        role="button"
+        @click="
+          () => {
+            expandDetailsSection = !expandDetailsSection;
+          }
+        "
+      >
+        <p class="text-sm font-medium">{{ placeholderExpanded }}</p>
+        <div class="flex items-center">
+          <svg
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            transform="rotate(180)"
+          >
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M6.351 8.751a1.2 1.2 0 0 1 1.698 0L12 12.703l3.951-3.952a1.2 1.2 0 1 1 1.697 1.697l-4.8 4.8a1.2 1.2 0 0 1-1.697 0l-4.8-4.8a1.2 1.2 0 0 1 0-1.697Z"
+              fill="#303339"
+            />
+          </svg>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "ExpandOnTap",
+  data() {
+    return {
+      expandDetailsSection: false,
+    };
+  },
   props: {
     text: {
       type: String,
       default: "",
     },
+    placeholderClosed: {
+      type: String,
+      default: "Tap to expand",
+    },
+    placeholderExpanded: {
+      type: String,
+      default: "Tap to close",
+    },
+    previewHeight: {
+      type: Number,
+      default: 120,
+    },
+    exapndOpenSectionStyle: {
+      type: String,
+      default: "",
+    },
+    exapndCloseSectionStyle: {
+      type: String,
+      default: "",
+    },
+  },
+
+  computed: {
+    previewHeightCss() {
+      return {
+        height: this.expandDetailsSection ? "100%" : this.previewHeight + "px",
+      };
+    },
   },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+.py-4 {
+  padding-top: 1rem;
+  padding-bottom: 1rem;
+}
+.w-full {
+  width: 100%;
+}
+.absolute {
+  position: absolute;
+}
+
+.bottom-0 {
+  bottom: 0px;
+}
+.flex {
+  display: flex;
+}
+.justify-center {
+  justify-content: center;
+}
+
+.items-center {
+  align-items: center;
+}
+
+.bg-gradient-fade {
+  background: linear-gradient(
+    187deg,
+    rgba(255, 255, 255, 0) -148.96%,
+    #fafafa 101.04%
+  );
+}
+
+.relative {
+  position: relative;
+}
+
+.overflow-clip {
+  overflow: clip;
+}
+</style>
