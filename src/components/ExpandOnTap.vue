a96a8cce3ef3225153987ab16578dc42538cafdd
ShababulAlam <sababul37@gmail.com>
2023-06-09 16:12:23 +0600

    Add: class support

--- src/components/ExpandOnTap.vue
+++ src/components/ExpandOnTap.vue
@@ -2,65 +2,34 @@
   <div>
     <div :style="previewHeightCss" class="exanpdable relative overflow-clip">
       <slot />
-      <div
-        v-if="!expandDetailsSection"
-        class="absolute bottom-0 w-full bg-gradient-fade flex justify-center"
-        :style="expandOpenSectionStyle"
-        role="button"
-        @click="
-          () => {
-            expandDetailsSection = !expandDetailsSection;
-          }
-        "
-      >
+      <div v-if="!expandDetailsSection" class="absolute bottom-0 w-full bg-gradient-fade flex justify-center"
+        :style="expandOpenSectionStyle" :class="expandOpenSectionClass" role="button" @click="() => {
+          expandDetailsSection = !expandDetailsSection;
+        }
+          ">
         <p class="">
           {{ placeholderClosed }}
         </p>
         <div class="flex items-center">
-          <svg
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fill-rule="evenodd"
-              clip-rule="evenodd"
+          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd" clip-rule="evenodd"
               d="M6.351 8.751a1.2 1.2 0 0 1 1.698 0L12 12.703l3.951-3.952a1.2 1.2 0 1 1 1.697 1.697l-4.8 4.8a1.2 1.2 0 0 1-1.697 0l-4.8-4.8a1.2 1.2 0 0 1 0-1.697Z"
-              fill="#303339"
-            />
+              fill="#303339" />
           </svg>
         </div>
       </div>
 
-      <div
-        v-else
-        class="w-full flex justify-center"
-        :style="expandCloseSectionStyle"
-        role="button"
-        @click="
-          () => {
-            expandDetailsSection = !expandDetailsSection;
-          }
-        "
-      >
+      <div v-else class="w-full flex justify-center" :style="expandCloseSectionStyle" role="button" @click="() => {
+        expandDetailsSection = !expandDetailsSection;
+      }
+        ">
         <p class="text-sm font-medium">{{ placeholderExpanded }}</p>
         <div class="flex items-center">
-          <svg
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-            transform="rotate(180)"
-          >
-            <path
-              fill-rule="evenodd"
-              clip-rule="evenodd"
+          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"
+            transform="rotate(180)">
+            <path fill-rule="evenodd" clip-rule="evenodd"
               d="M6.351 8.751a1.2 1.2 0 0 1 1.698 0L12 12.703l3.951-3.952a1.2 1.2 0 1 1 1.697 1.697l-4.8 4.8a1.2 1.2 0 0 1-1.697 0l-4.8-4.8a1.2 1.2 0 0 1 0-1.697Z"
-              fill="#303339"
-            />
+              fill="#303339" />
           </svg>
         </div>
       </div>
@@ -74,7 +43,7 @@ export default {
   data() {
     return {
       expandDetailsSection: false,
-    };
+    }
   },
   props: {
     text: {
@@ -101,26 +70,36 @@ export default {
       type: String,
       default: "",
     },
+    expandOpenSectionClass: {
+      type: String,
+      default: "",
+    },
+    expandCloseSectionClass: {
+      type: String,
+      default: "",
+    },
   },
 
   computed: {
     previewHeightCss() {
       return {
         height: this.expandDetailsSection ? "100%" : this.previewHeight + "px",
-      };
+      }
     },
   },
-};
+}
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .py-4 {
   padding-top: 1rem;
   padding-bottom: 1rem;
 }
+
 .w-full {
   width: 100%;
 }
+
 .absolute {
   position: absolute;
 }
@@ -128,9 +107,11 @@ export default {
 .bottom-0 {
   bottom: 0px;
 }
+
 .flex {
   display: flex;
 }
+
 .justify-center {
   justify-content: center;
 }
@@ -140,11 +121,9 @@ export default {
 }
 
 .bg-gradient-fade {
-  background: linear-gradient(
-    187deg,
-    rgba(255, 255, 255, 0) -148.96%,
-    #fafafa 101.04%
-  );
+  background: linear-gradient(187deg,
+      rgba(255, 255, 255, 0) -148.96%,
+      #fafafa 101.04%);
 }
 
 .relative {
